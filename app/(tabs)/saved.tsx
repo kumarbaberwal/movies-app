e9beeeee7d928a495d48d028e425454e4e1f0853
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-03-26 00:19:37 +0530

    movies-app

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Index() {
+export default function Saved() {
   return (
     <View>
-      <Text>Index</Text>
+      <Text>Saved</Text>
     </View>
   )
 }
---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 justify-center items-center"
+    >
+      <Text
+        className="text-2xl text-primary font-bold"
+      >
+        Welcome Chetna
+      </Text>
+    </View>
+  );
+}
