563863aec6f9f5e2b801b440701d724a9211929e
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-03-27 23:15:10 +0530

    tabs layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,8 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, Image } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { images } from '@/constants/images'
+import { icons } from '@/constants/icons'
 
 export default function TabsLayout() {
   return (
@@ -9,14 +11,25 @@ export default function TabsLayout() {
         name='index'
         options={{
           headerShown: false,
-          title: 'Home'
-        }}
-      />
-      <Tabs.Screen
-        name='search'
-        options={{
-          title: 'Search',
-          headerShown: false,
+          title: 'Home',
+          tabBarIcon: ({ focused }) => (
+            <>
+              <ImageBackground
+                source={images.highlight}
+                className='flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'
+              >
+                <Image
+                  source={icons.home}
+                  className='size-5'
+                />
+                <Text
+                  className='text-secondary text-base font-semibold ml-2'
+                >
+                  Home
+                </Text>
+              </ImageBackground>
+            </>
+          )
         }}
       />
       <Tabs.Screen
