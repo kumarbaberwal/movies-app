5f84cc6f387ff984e79cddbfc0e106215b07fd78
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-03-31 23:41:37 +0530

    Home Screen - UI, SearchBar - component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,43 @@
-import { Text, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
+import { Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
   return (
     <View
-      className="flex-1 justify-center items-center"
+      className="flex-1 bg-primary"
     >
-      <Text
-        className="text-2xl text-primary font-bold"
+      <Image
+        source={images.bg}
+        className="absolute w-full z-0"
+        resizeMode="contain"
+      />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: '100%',
+          paddingBottom: 10,
+        }}
       >
-        Welcome Kumar...
-      </Text>
+        <Image
+          source={icons.logo}
+          className="w-12 h-10 mt-20 mb-5 mx-auto"
+        />
+        <View
+          className="flex-1 mt-5"
+        >
+          <SearchBar
+            onPress={() => {
+              router.push('/(tabs)/search')
+            }}
+            placeholder="Search for a movie"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
