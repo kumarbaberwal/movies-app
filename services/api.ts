d327f314f27784c3c54a5ed0f3c57d9f1a69568a
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-04-02 13:17:18 +0530

    Custom Hook - useFetch (api.ts)

---
+++ services/api.ts
@@ -0,0 +1,45 @@
+export const TMDB_CONFIG = {
+    BASE_URL: 'https://api.themoviedb.org/3',
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+    }
+}
+
+export const fetchMovies = async ({ query }: { query: string }) => {
+    const endPoint = query
+        ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=` + query
+        : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+
+    const response = await fetch(endPoint, {
+        method: 'GET',
+        headers: TMDB_CONFIG.headers,
+    })
+
+    if (!response.ok) {
+        // @ts-ignore
+        throw new Error('Failed to fetch movies', response.statusText);
+    }
+
+    const data = await response.json();
+    return data.results;
+}
+
+
+
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         accept: 'application/json',
+//         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNmZmMjVkM2RjNWE0NjhkODE1OGI5YjE5Zjk2YzcxZiIsIm5iZiI6MTczMTIyMTYxOC4wOTMsInN1YiI6IjY3MzA1ODcxYzMxYTVkY2JiMjE0YWNlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._3dqgU2d3K1AibgPlAkRIYyXyTO-PS1fEzkXcxxleV8'
+//     }
+// };
+
+// fetch(url, options)
+//     .then(res => res.json())
+//     .then(json => console.log(json))
+//     .catch(err => console.error(err));
+
---
+++ services/useFetch.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = (fetchFunction: () => Promise<T>, autoFetch: true) => {
+    const [data, setData] = useState<T | null>(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<Error | null>(null);
+    const fetchData = async () => {
+        try {
+            setLoading(true);
+            const data = await fetchFunction();
+            setData(data);
+        } catch (error) {
+            setError(error instanceof Error ? error : new Error('An error occurred'));
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const reset = async () => {
+        setData(null);
+        setLoading(false);
+        setError(null);
+    };
+
+
+    useEffect(() => {
+        if (autoFetch) {
+            fetchData();
+        }
+    }, []);
+
+    return { data, loading, error, refetch: fetchData, reset };
+}
