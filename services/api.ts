abf821686a34df33978321cbe36ba27625e87a8d
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-04-07 18:28:09 +0530

    api.ts - fetchMovieDetails

--- services/api.ts
+++ services/api.ts
@@ -43,3 +43,23 @@ export const fetchMovies = async ({ query }: { query: string }) => {
 //     .then(json => console.log(json))
 //     .catch(err => console.error(err));
 
+
+export const fetchMovieDetails = async (movieId: string): Promise<MovieDetails> => {
+    try {
+        const response = await fetch(`${TMDB_CONFIG.BASE_URL}/moive/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`, {
+            method: 'GET',
+            headers: TMDB_CONFIG.headers,
+        });
+
+
+        if (!response.ok) {
+            // @ts-ignore
+            throw new Error('Failed to fetch movie details', response.statusText);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}   
