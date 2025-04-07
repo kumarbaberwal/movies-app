d4fa069bee95d311609ad69bec30697da3091ca9
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-04-08 03:07:00 +0530

    profile.tsx, saved.tsx

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { icons } from '@/constants/icons'
 
 export default function Saved() {
   return (
-    <View>
-      <Text>Saved</Text>
+    <View
+      className='flex-1 bg-primary px-10'
+    >
+      <View
+        className='flex justify-center items-center flex-1 flex-col gap-5'
+      >
+        <Image
+          source={icons.save}
+          className='size-10'
+          tintColor={'#fff'}
+        />
+        <Text
+          className='text-gray-500 text-base'
+        >
+          Save
+        </Text>
+      </View>
     </View>
   )
 }
